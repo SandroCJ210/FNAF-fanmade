6357a2dbf372de9e3533a33fd75b18b4fc2eea28
SandroCJ210 <sandro.carrillo.j@uni.pe>
2024-04-23 23:38:10 -0500

    small update on dead

--- Assets/Scripts/Game/Animatronics/Chica.cs
+++ Assets/Scripts/Game/Animatronics/Chica.cs
@@ -4,18 +4,33 @@ using UnityEngine;
 
 public class Chica : Animatronic
 {
-    private void Start() 
+    [SerializeField] private DoorButton doorButton;
+    private void Start()
     {
+        StartCoroutine(MovementOportunity());
         waypoints[currentRoom].ChicaInside = true;  
     }
 
     protected override bool CheckDoorOpen()
     {
-        return true;
+        return doorButton.IsOpen;
     }
 
     protected override void Move()
     {
-        
+        int randomNumber;
+        waypoints[currentRoom].ChicaInside = false;
+        switch (currentRoom)
+        {
+            case 1:
+                currentRoom++;
+                break;
+            default:
+                randomNumber = Random.Range(0,2) + 1;
+                currentRoom += randomNumber;
+                break;
+                
+        }
+        waypoints[currentRoom].ChicaInside = true;
     }
 }
---
+++ Assets/Scripts/Game/JumpscareTrigger.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpscareTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject lineAnimation;
+    [SerializeField] private GameObject staticAnimation;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void EndOfJumpscare()
+    {
+        audioSource.Stop();
+        Instantiate(staticAnimation, Camera.main.transform.position + new Vector3(0,0,10), Quaternion.identity);
+        Instantiate(lineAnimation, Camera.main.transform.position + new Vector3(0,0,10), Quaternion.identity);
+    }
+}
